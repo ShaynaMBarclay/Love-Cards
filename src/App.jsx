6cbb1a3f2dd4eb942a3b43453b8a6ba514f1f858
ShaynaMBarclay <shaynambarclay@gmail.com>
2026-02-10 10:23:08 -0500

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+import cuteImg from "./assets/kiss.gif";
+
+export default function App() {
+  const [accepted, setAccepted] = useState(false);
+  const [showWrong, setShowWrong] = useState(false);
+
+  const handleNo = () => {
+    setShowWrong(true);
+    setTimeout(() => setShowWrong(false), 1200);
+  };
+
+  return (
+    <div className="page">
+      <div className="card">
+        {accepted ? (
+          <>
+            <h1 className="title">YAYYY 💕</h1>
+
+            <p className="message">
+              You just made me the happiest human alive 🥺💗  
+              Happy Valentine’s Day my love ✨
+            </p>
+
+            <img src={cuteImg} alt="cute" className="cute-img" />
+          </>
+        ) : (
+          <>
+            <h1 className="title">Will you be my Valentine? 💌</h1>
+
+            <div className="buttons">
+              <button
+                className="yes-btn"
+                onClick={() => setAccepted(true)}
+              >
+                Yes 💖
+              </button>
+
+              <button
+                className="no-btn"
+                onMouseEnter={handleNo}
+                onClick={handleNo}
+              >
+                No
+              </button>
+            </div>
+          </>
+        )}
+
+        {showWrong && <div className="popup">WRONG ANSWER ❌</div>}
+      </div>
+    </div>
+  );
+}
